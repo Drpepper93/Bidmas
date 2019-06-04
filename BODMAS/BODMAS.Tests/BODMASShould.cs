c8cf984d23fea05f61e75830de654d091d231d84
Drpepper93 <51212553+Drpepper93@users.noreply.github.com>
2019-06-04 17:16:53 +0100

    Add files via upload
    Some unit tests added and bug fixes made.

---
+++ BODMAS/BODMAS.Tests/BODMASShould.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BODMAS.Tests
+{
+    [TestFixture]
+    class BODMASShould
+    {
+        [Test]
+        public void CalcCorrectAnswer()
+        {
+            string eq = "2*4/8+1-8*22-4";
+            var ans = BODMAS.BIDMAS(eq);
+            Assert.That(ans, Is.EqualTo(Convert.ToDouble(-178)));
+
+            string eq2 = "25*25/3*4^2-22+46-200000+3";
+            var ans2 = BODMAS.BIDMAS(eq2);
+            Assert.That(ans2, Is.EqualTo(Convert.ToDouble(-196639.666666667)));
+
+            string eq3 = "4*8/-22*-78+55-22+3";
+            var ans3 =BODMAS.BIDMAS(eq3);
+            Assert.That(ans3, Is.EqualTo(Convert.ToDouble(149.454545454546)));
+        }
+    }
+}
--- BODMAS/BODMAS/Program.cs
+++ BODMAS/BODMAS/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Text;
 using System.Data;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace BODMAS
 {
-    class Program
+    public class BODMAS
     {
         static void Main(string[] args)
         {
-            string strin = "2+5^2/4*4+3";
-            BIDMAS(strin);
-
+            var a = BIDMAS("56+4-2+4/-5/5*6/5*9");
+            Console.WriteLine(a);
         }
         // BIDMAS takes a string equation and then displays the answer
-        static void BIDMAS(string equation)
+        public static double BIDMAS(string equation)
         {
             StringBuilder sb = new StringBuilder(equation);
             string math;
@@ -27,36 +28,37 @@ namespace BODMAS
                     {
                         math = Replacalc(ch, i, sb);
                         sb = new StringBuilder(math);
+                        i = 0;
                     }
                     if (ch == "/" & j == 1)
                     {
                         math = Replacalc(ch, i, sb);
                         sb = new StringBuilder(math);
+                        i = 0;
                     }
                     if (ch == "*" & j == 2)
                     {
                         math = Replacalc(ch, i, sb);
                         sb = new StringBuilder(math);
+                        i = 0;
                     }
                     if ((ch =="+"||ch=="-")&j==3)
                     {
                         math = Replacalc(ch, i, sb);
                         sb = new StringBuilder(math);
-                    }
+                        i = 0;                    }
                 }
                 j++;
             }
-
             double answer = double.Parse(sb.ToString());
-            Console.WriteLine("Answer: {0}",answer);
-
+            return(answer);
         }
         // Replacalc takes the character of an operator its position and the whole StringBuilder the
         // operator is in. As a result it produces a string containing the equation but with the numbers
         // either side of the operator calculated.
         // i.e. input:      ("^",3,"2+5^2/4*4+3")
         //      output:     "2+25/4*4+3"
-        static string Replacalc(string ch, int inde, StringBuilder sb)
+        public static string Replacalc(string ch, int inde, StringBuilder sb)
         {
             int count = 0;
             int index1 = 0;
@@ -74,26 +76,23 @@ namespace BODMAS
                 {
                     sbch = sb[j].ToString();
                 }
-                if (sbch != "^" & sbch != "/" & sbch != "*"& sbch != "+"& sbch != "-"&j!=-1)
+                if (sbch != "^" & sbch != "/" & sbch != "*"& sbch != "+"&j!=-1)
                 {
                     count++;
+                    if(sbch=="-")
+                    {
+                        j--;
+                        index1 = j+1;
+                        x = Nbo(sb, j, count);
+                        break;
+                    }
                 }
                 else
-                { 
-                    StringBuilder sb2 = new StringBuilder("");
-                    for (int q = 1; q < count + 1; q++)
-                    {
-                        string dig = sb[j + q].ToString();
-                        sb2.Append(dig);
-                    }
-                    double int1 = double.Parse(sb2.ToString());
-                    x = int1;
-                    Console.WriteLine(int1);
+                {
                     index1 = j + 1;
+                    x = Nbo(sb, j, count);
                     break;
                 }
-                
-
             }
             int count2 = 0;
             for (int j = inde + 1; j < sb.Length+1; j++)
@@ -107,25 +106,20 @@ namespace BODMAS
                 {
                     sbch = sb[j].ToString();
                 }
-                if (sbch != "^" & sbch != "/" & sbch != "*" & sbch != "+" & sbch != "-"& j !=sb.Length)
+                if (sbch == "-" & count2 == 0)
+                {
+                    count2++;
+                }
+                else if(sbch != "^" & sbch != "/" & sbch != "*" & sbch != "+" & j !=sb.Length & sbch!="-")
                 {
                     count2++;
                 }
                 else
                 {
-                    StringBuilder sb2 = new StringBuilder("");
-                    for (int q = 1; q < count2 + 1; q++)
-                    {
-                        string dig = sb[j - (count2 + 1) + q].ToString();
-                        sb2.Append(dig);
-                    }
-                    double int2 = double.Parse(sb2.ToString());
-                    y = int2;
-                    Console.WriteLine(int2);
+                    y = Nao(sb, j, count2);
                     index2 = j - 1;
                     break;
                 }
-                
             }
             string math = "";
             if (ch == "^") { math = Pow(x, y, index1, index2, sb); }
@@ -134,13 +128,12 @@ namespace BODMAS
             if (ch== "+") { math = Plu(x, y, index1, index2, sb); }
             if (ch == "-") { math = Min(x, y, index1, index2, sb); }
             return math;
-            
         }
         // Div takes the number before (x) and after (y) the operator, the index of the start (index1) and
         // end (index2) of the sub-equation, and the whole StringBuilder. i.e.
         // input:       (25,4,2,5,"2+25/4*4-3")
         // output:      "2+6.25*4-3"
-        static string Div(double x, double y, int index1, int index2, StringBuilder sb)
+        public static string Div(double x, double y, int index1, int index2, StringBuilder sb)
         {
             double ans = x / y;
             sb.Remove(index1, index2 - index1 + 1);
@@ -151,7 +144,7 @@ namespace BODMAS
         // end (index2) of the sub-equation, and the whole StringBuilder. i.e.
         // input:       (6.25,4,2,7,"2+6.25*4-3")
         // output:      "2+25-3"
-        static string Tim(double x, double y, int index1, int index2, StringBuilder sb)
+        public static string Tim(double x, double y, int index1, int index2, StringBuilder sb)
         {
             double ans = x * y;
             sb.Remove(index1, index2 - index1 + 1);
@@ -162,14 +155,13 @@ namespace BODMAS
         // end (index2) of the sub-equation, and the whole StringBuilder. i.e.
         // input:       (5,2,2,4,"2+5^2/4*4-3")
         // output:      "2+25/4*4-3"
-        static string Pow(double x, double y, int index1, int index2, StringBuilder sb)
+        public static string Pow(double x, double y, int index1, int index2, StringBuilder sb)
         {
             double ans = x;
             for (int i = 1; i < y; i++)
             {
-                ans = ans * ans;
+                ans *= ans;
             }
-
             sb.Remove(index1, index2 - index1 + 1);
             sb.Insert(index1, ans);
             return sb.ToString();
@@ -178,7 +170,7 @@ namespace BODMAS
         // end (index2) of the sub-equation, and the whole StringBuilder. i.e.
         // input:       (2,25,0,3,"2+25-3")
         // output:      "27-3"
-        static string Plu(double x, double y, int index1, int index2, StringBuilder sb)
+        public static string Plu(double x, double y, int index1, int index2, StringBuilder sb)
         {
             double ans = x + y;
             sb.Remove(index1, index2 - index1 + 1);
@@ -189,12 +181,36 @@ namespace BODMAS
         // end (index2) of the sub-equation, and the whole StringBuilder. i.e.
         // input:       (27,3,0,3,"27-3")
         // output:      "24"
-        static string Min(double x, double y, int index1, int index2, StringBuilder sb)
+        public static string Min(double x, double y, int index1, int index2, StringBuilder sb)
         {
             double ans = x - y;
             sb.Remove(index1, index2 - index1 + 1);
             sb.Insert(index1, ans);
             return sb.ToString();
+        }
+        //Nbo finds the number before the operator
+        public static double Nbo(StringBuilder sb, int j,int count)
+        {
+            StringBuilder sb2 = new StringBuilder("");
+            for (int q = 1; q < count + 1; q++)
+            {
+                string dig = sb[j + q].ToString();
+                sb2.Append(dig);
+            }
+            double x = double.Parse(sb2.ToString());
+            return x;
+        }
+        //Nao finds the number after the operator
+        public static double Nao(StringBuilder sb, int j, int count2)
+        {
+            StringBuilder sb2 = new StringBuilder("");
+            for (int q = 1; q < count2 + 1; q++)
+            {
+                string dig = sb[j - (count2 + 1) + q].ToString();
+                sb2.Append(dig);
+            }
+            double y = double.Parse(sb2.ToString());
+            return y;
         }
     }
 }
